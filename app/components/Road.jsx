33dcd68242b805db3ee2d21608cb35ac8e9f8ddd
Riteshk_007 <codeshorts007@gmail.com>
2024-07-08 16:35:31 +0530

    refactor: Improve Road component styling and add checkmark button

--- app/components/Road.jsx
+++ app/components/Road.jsx
@@ -1,17 +1,17 @@
 "use client";
-import { Check, ChevronDown } from "lucide-react";
+import { Check, ChevronDown, LockKeyhole } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 const Road = () => {
   const [open, setOpen] = React.useState(false);
   return (
-    <div className="w-full  flec flex-col items-center justify-center bg-gradient-to-b from-black to-[#151415] p-5 h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-2  ">
+    <div className="w-full  flex flex-col items-center justify-center bg-gradient-to-b from-black to-[#151415] p-5 lg:h-screen">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
         {/* 1st card */}
         <div
           onClick={() => setOpen(!open)}
-          className="p-5 bg-[#151415]/50  cursor-pointer backdrop-blur-md w-[500px] shadow-lg mx-auto transition-all duration-200 hover:bg-[#1d1b1d] rounded-lg relative flex items-center justify-center flex-col"
+          className="md:p-5 bg-[#151415]/50  h-min cursor-pointer backdrop-blur-md w-auto md:w-[500px] shadow-lg mx-auto transition-all duration-200 hover:bg-[#1d1b1d] rounded-lg relative flex items-center justify-center flex-col"
         >
           <div className=" p-4 rounded-lg  flex items-center space-x-4 w-full">
             <div className="w-full ">
@@ -52,36 +52,36 @@ const Road = () => {
             }`}
           />
           {!open && (
-            <div className="bg-[#222122] p-4">
-              <div className="h-80 overflow-y-auto space-y-4">
+            <div className=" border-t-2 border-[#222122] p-4">
+              <div className="h-80 overflow-y-auto space-y-4 overflow-x-auto">
                 {cards.map((card, index) => (
                   <div
                     key={index}
-                    className="flex items-center p-4 rounded-lg shadow-md bg-[#151415] relative"
+                    className="flex flex-col sm:flex-row items-center p-2 sm:p-4 rounded-lg shadow-md bg-[#2D2D2D] hover:bg-[#1a1a1a] relative"
                   >
                     <Image
                       width={100}
                       height={100}
                       src={card.img}
                       alt={card.title}
-                      className="w-24 h-16 object-cover rounded-lg mr-4"
+                      className="w-16 h-10 sm:w-24 sm:h-16 object-cover rounded-lg mr-2 sm:mr-4"
                     />
-                    <div className="flex-1 min-w-0">
-                      <div className="text-lg font-semibold truncate text-gray-300">
+                    <div className="flex-1 min-w-0 mt-2 sm:mt-0">
+                      <div className="text-md sm:text-lg font-semibold truncate text-gray-300">
                         {card.title}
                       </div>
-                      <div className="text-sm text-muted-foreground text-gray-400">
+                      <div className="text-xs sm:text-sm text-muted-foreground text-gray-400">
                         {card.task}
                       </div>
-                      <hr className="border-b border-dashed border-[#3a3a3a] mt-2 mb-5" />
+                      <hr className="border-b border-dashed border-[#3a3a3a] mt-2 mb-3 sm:mb-5" />
                       <div className="mt-2">
                         <div className="w-full flex items-center justify-center h-2 rounded-full gap-2">
                           <div
                             className="bg-[#1f1e1f] h-3 rounded-full border-2 border-[#545252]"
                             style={{ width: "100%" }}
                           ></div>
-                          <button className=" flex items-center justify-center p-1 border border-[#4f4e4e] bg-[#383838] rounded-full text-[#545454] ">
-                            <Check className="h-4 w-4" />
+                          <button className="flex items-center justify-center p-1 border border-[#4f4e4e] bg-[#383838] rounded-full text-[#545454] ">
+                            <Check className="h-3 w-3 sm:h-4 sm:w-4" />
                           </button>
                         </div>
                       </div>
@@ -93,7 +93,38 @@ const Road = () => {
           )}
         </div>
         {/* 2nd card */}
-        <div className="w-full h-full border"></div>
+        <div className="w-full h-full  flex items-center justify-center sm:pl-52 pt-40">
+          <div className="relative flex items-center flex-col justify-center">
+            <span className="bg-[#1e1d1e] p-1 my-4 rounded-full shadow-lg border-[#333333] border backdrop-blur-md">
+              <Check className="h-10 w-10 text-[#323132]" />
+            </span>
+            <div className="relative bg-[#1e1d1e] p-1 my-4 rounded-2xl">
+              <Image
+                src={
+                  "https://static.highongrowth.xyz/erc1155-images/65017d014e5f24613adbfd67/1b1912563b964393b0478949efad5ed3.png"
+                }
+                width={150}
+                height={100}
+                alt="intract-certified"
+                className=" rounded-2xl"
+              />
+              <span className="absolute -bottom-4 -right-3 bg-[#1e1d1e] p-3 rounded-full shadow-lg border-[#333333] border backdrop-blur-md">
+                <LockKeyhole className="h-5 w-5 text-[#5f5f5f]" />
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <h1 className="text-lg font-semibold text-gray-400 text-center">
+                Intract Certified: Learner NFT
+              </h1>
+              <p className="text-sm flex-wrap text-gray-600 font-semibold">
+                Your crypto black-belt <br /> certificate
+              </p>
+              <button className="bg-[#1e1d1e] text-gray-400 font-semibold py-2 px-4 rounded-lg my-4  cursor-not-allowed  shadow-lg border border-[#262626] ">
+                Claim
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
