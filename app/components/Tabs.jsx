18746c767fa671b6da0f1346e2ae514761a4b4de
Riteshk_007 <codeshorts007@gmail.com>
2024-07-09 14:09:40 +0530

    refactor: Improve Tabs component functionality and add smooth scrolling to top

--- app/components/Tabs.jsx
+++ app/components/Tabs.jsx
@@ -4,25 +4,34 @@ import { useState } from "react";
 
 const Tabs = () => {
   const [activeTab, setActiveTab] = useState("Essentials");
+
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="sticky bottom-5 flex my-5 shadow-2xl bg-gray-800/50 backdrop-blur-md rounded-full mx-auto w-60">
       <button
-        className={`transition duration-300 px-5 rounded-full  ease-in-out transform hover:scale-105 py-2 ${
+        className={`transition duration-300 px-5 rounded-full ease-in-out transform hover:scale-105 py-2 ${
           activeTab === "Essentials"
-            ? "font-bold bg-gray-700/60 text-white shadow-lg ackdrop-blur-md"
+            ? "font-bold bg-gray-700/60 text-white shadow-lg backdrop-blur-md"
             : "bg-secondary"
         }`}
-        onClick={() => setActiveTab("Essentials")}
+        onClick={() => handleTabChange("Essentials")}
       >
         Essentials
       </button>
       <button
-        className={`transition duration-300 ease-in-out transform hover:scale-105  px-5 rounded-full py-2 ${
+        className={`transition duration-300 ease-in-out transform hover:scale-105 px-5 rounded-full py-2 ${
           activeTab === "Alpha Hub"
-            ? "font-bold bg-gray-700/60 text-white shadow-lg ackdrop-blur-md"
+            ? "font-bold bg-gray-700/60 text-white shadow-lg backdrop-blur-md"
             : "bg-secondary"
         }`}
-        onClick={() => setActiveTab("Alpha Hub")}
+        onClick={() => handleTabChange("Alpha Hub")}
       >
         Alpha Hub
       </button>
